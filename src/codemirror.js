841e2d13ef3a960b151d6e106f8805cbd69c3ed2
Juan Cruz Leyba <jleyba@itx.com>
2019-10-22 13:05:33 -0300

    Adding supported languages map and CodeMirror autocomplete feature

--- src/codemirror.js
+++ src/codemirror.js
@@ -9,15 +9,54 @@ export default function CodeMirrorReact({
 }) {
   const inputElement = useRef();
 
-  require(`codemirror/mode/${language}/${language}.js`);
-
   if (options.theme && !customTheme) {
     require(`codemirror/theme/${options.theme}.css`);
   }
 
   useEffect(() => {
-    CodeMirror.fromTextArea(inputElement.current, options);
-  }, [options]);
+    CodeMirror.fromTextArea(
+      inputElement.current,
+      getOptions(options, language)
+    );
+  }, [options, language]);
 
   return <textarea ref={inputElement} defaultValue={""} />;
+}
+
+const supportedLanguages = {
+  html: "htmlmixed",
+  javascript: "javascript",
+  python: "python",
+  css: "css",
+  sql: "sql"
+};
+
+function getOptions(options, language) {
+  const mode = supportedLanguages[language];
+
+  let optionsCopy = { ...options };
+
+  // Checking if language is supported
+  if (mode) {
+    require(`codemirror/mode/${mode}/${mode}.js`);
+    optionsCopy = { ...optionsCopy, mode };
+  } else {
+    throw new Error("Language mode not supported");
+  }
+
+  // Configure hint for autocomplete
+  if (options.hint) {
+    require("codemirror/addon/hint/show-hint.js");
+    require("codemirror/addon/hint/show-hint.css");
+
+    // Getting hint function from path
+    const path =
+      language === supportedLanguages.python
+        ? "./python-hint.js"
+        : `codemirror/addon/hint/${language}-hint.js`;
+    const hint = require(`${path}`);
+    optionsCopy = { ...optionsCopy, hint };
+  }
+
+  return optionsCopy;
 }
